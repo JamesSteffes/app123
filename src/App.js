3a64f605cc11fe6d57b0055eccf741e6d87ea4b9
wither7007 <jimsteffes@outlook.com>
2022-02-15 16:44:37 -0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My React</h1>
+        <Header />
+        <h1></h1>
       </header>
     </div>
   );
