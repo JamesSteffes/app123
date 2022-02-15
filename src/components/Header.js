48a945c81950d490b58bfa63e92c953d5d766502
wither7007 <jimsteffes@outlook.com>
2022-02-15 16:44:47 -0600

    ok

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            Header
+            from new components
+
+        </div>
+    )
+}
+
+export default Header
